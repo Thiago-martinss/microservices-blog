0a26651201ab7017bd776339d9b557658daa3f71
Thiago <martinsmthiago@gmail.com>
2025-01-12 21:57:43 -0300

    initial commit

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+        content,
+      });
+      setContent('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
---
+++ client/src/PostList.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
+
+export default () => {
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get('http://localhost:4000/posts');
+      setPosts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => {
+    return (
+    <div 
+    className="card" style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
+      <div className="card-body">
+        <h5 className="card-title">{post.title}</h5>
+        <CommentList postId={post.id} />
+        <CommentCreate postId={post.id} />
+        <p className="card-text">{post.content}</p>
+      </div>
+      <a href={`/posts/${post.id}`} className="btn btn-primary">Read More</a>
+    </div>
+    );
+  });
+
+  return <div className='d-flex flex-row flex-wrap justify-content-between'>
+    
+    {renderedPosts}
+  </div>;
+};
